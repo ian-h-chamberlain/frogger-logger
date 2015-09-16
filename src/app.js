4bb40a1c490bf5e4eced0570838cce003d2d07e2
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2015-09-15 21:47:28 -0400

    check for off-screen logs and delete them, and change how velocity works

--- src/app.js
+++ src/app.js
@@ -6,17 +6,29 @@ var LogLayer = cc.Layer.extend({
         // 1. super init first
         this._super();
 
+        // an array to hold the logs
         this.logs = [];
 
+        // schedule the update function to run
         this.scheduleUpdate();
 
         return true;
     },
 
     update: function(dt) {
+        // spawn logs randomly
+        // TODO: check for other logs in spawn location and determine when to spawn logs
         if (Math.random() > 0.99) {
             this.logs.push(new Log());
             this.addChild(this.logs[this.logs.length-1]);
+        }
+
+        // check logs for being off-screen and delete them if so
+        for (var i=0; i<this.logs.length; i++) {
+            if ((this.logs[i].x - this.logs[i].width/2) > cc.winSize.width) {
+                delete this.logs[i];
+                this.logs.splice(i, 1);
+            }
         }
     }
 });
--- src/logs.js
+++ src/logs.js
@@ -2,15 +2,19 @@ var Log = cc.Sprite.extend({
     ctor: function() {
         this._super(res.log_png);
 
-        this.x = -40;
+        this.x = -this.width;
         this.y = Math.floor((Math.random() * cc.winSize.height) + 1);   // randomly spawn on-screen
 
-        this.runAction(new cc.MoveTo(30, cc.p(cc.winSize.width + 40, this.y)));
+        // X and Y velocity, in log-units per second
+        this.velX = 1.6;
+        this.velY = 0;
 
         this.scheduleUpdate();
     },
 
     update: function(dt) {
-        ; // do nothing for now
+        // calculate where we're headed
+        this.x += this.velX * this.width * dt;
+        this.y += this.velY * this.width * dt;
     }
 });
