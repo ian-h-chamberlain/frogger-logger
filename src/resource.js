81710b45e9f1b3d6f739fe17f420f84b5eca1ffa
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2015-09-16 14:25:18 -0400

    Fixed error messages in console

--- src/resource.js
+++ src/resource.js
@@ -1,9 +1,6 @@
 var res = {
     log_png: "res/log.png",
-    longLog_png: "res/longlog.png"
-    HelloWorld_png : "res/HelloWorld.png",
-    CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png",
+    longLog_png: "res/longlog.png",
     
     CompassLogo_png : "res/CompassLogo.png",
     RpiMono_png : "res/RPIMONO.png",
