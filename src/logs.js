106cfc79110108d576858b8e605efde5166e64a2
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2015-09-16 14:17:13 -0400

    Moved LogLayer into logs.js

--- src/logs.js
+++ src/logs.js
@@ -1,3 +1,59 @@
+var LogLayer = cc.Layer.extend({
+    sprite:null,
+
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        // an array to hold the logs
+        this.logs = [];
+
+        // schedule the update function to run
+        this.scheduleUpdate();
+
+        return true;
+    },
+
+    update: function(dt) {
+        // spawn logs randomly
+        // TODO: check for other logs in spawn location and determine when to spawn logs
+        if (Math.random() > 0.9) {
+            this.addLog();
+        }
+
+        // check logs for being off-screen and delete them if so
+        for (var i=0; i<this.logs.length; i++) {
+            if ((this.logs[i].x - this.logs[i].width/2) > cc.winSize.width) {
+                delete this.logs[i];
+                this.logs.splice(i, 1);
+            }
+        }
+    },
+
+    // addLog - adds a log randomly with bounds checking
+    addLog: function() {
+        var newLog;
+        if (Math.random() > 0.5)
+            newLog = new Log(res.log_png);
+        else
+            newLog = new LongLog(res.longLog_png);
+
+        for (var i=0; i<this.logs.length; i++) {
+            // ensure that the new log will not overlap an existing log
+            if (newLog.y >= this.logs[i].y - 1.5*this.logs[i].height
+                && newLog.y <= this.logs[i].y + 1.5*this.logs[i].height
+                && this.logs[i].x <= cc.winSize.width / 3) {
+                return;
+            }
+        }
+
+        // actually add the log
+        this.logs.push(newLog);
+        this.addChild(this.logs[this.logs.length - 1]);
+    }
+});
+
 var Log = cc.Sprite.extend({
     ctor: function(sprite) {
         this._super(sprite);
